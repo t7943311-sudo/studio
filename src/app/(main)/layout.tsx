8320977a4a0572c046e1f745e313fc99c03cd78f
Tech <t7943311@gmail.com>
2026-01-11 04:19:15 +0000

    Runtime Error Server
    Attempted to call useSidebar() from the server bu

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,4 +1,3 @@
-"use client";
 
 import { SidebarProvider } from '@/contexts/sidebar-provider';
 import { Header } from '@/components/layout/header';
@@ -11,14 +10,14 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <SidebarProvider>
-      <div className="flex min-h-screen w-full">
+    <div className="flex min-h-screen w-full">
+      <SidebarProvider>
         <Sidebar />
         <div className="flex flex-1 flex-col">
           <Header />
           <main className="flex-1 bg-background p-4 sm:p-6">{children}</main>
         </div>
-      </div>
-    </SidebarProvider>
+      </SidebarProvider>
+    </div>
   );
 }
